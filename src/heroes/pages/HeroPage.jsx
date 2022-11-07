4a57faa0759ac63c77bf7cbbca6823e90b3645ad
Adolfo <adlf.aldana@gmail.com>
2022-11-06 21:14:27 -0400

    #feat: useMemo

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,9 +1,10 @@
+import { useMemo } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../helpers/getHeroById";
 
 export const HeroPage = () => {
   const { id } = useParams();
-  const hero = getHeroById(id);
+  const hero = useMemo(() => getHeroById(id), [id]); // getHeroById(id);
   const navigate = useNavigate();
 
   const onNavigateBack = () => {
