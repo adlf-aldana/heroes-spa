595c5130dd20933a7c2205f06c392e4edc687746
Adolfo <adlf.aldana@gmail.com>
2022-11-06 20:44:06 -0400

    #feat: read arguments by url

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,3 +1,13 @@
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers/getHeroById";
+
 export const HeroPage = () => {
+  const { id } = useParams();
+  const hero = getHeroById(id);
+
+  if (!hero) {
+    return <Navigate to="/marvel" />;
+  }
+
   return <div>heroPage</div>;
 };
