bc5e9d405470b896aaaa00b355373b8335a1efaf
Adolfo <adlf.aldana@gmail.com>
2022-11-07 22:51:08 -0400

    #feat: keep user active

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,20 +4,24 @@ import { authReducer } from "./authReducer";
 import { types } from "../types/types";
 
 export const AuthProvider = ({ children }) => {
-  const initialState = {
-    logged: false,
+  const init = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    return {
+      logged: !!user,
+      user,
+    };
   };
 
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, {}, init);
 
   const login = (name = "") => {
+    const user = { id: "ABC", name };
     const action = {
       types: types.login,
-      payload: {
-        id: "ABC",
-        name: name,
-      },
+      payload: user,
     };
+    localStorage.setItem("user", JSON.stringify(user));
     dispatch(action);
   };
 
